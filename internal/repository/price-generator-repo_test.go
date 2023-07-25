3e01e2bd66fc40ab338c3cdc7227741d24bf99f6
Oleg Minovich <oleg.minovich@mail.ru>
2023-07-25 15:42:55 +0300

    test for AddToStream

---
+++ internal/repository/price-generator-repo_test.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddToStream(t *testing.T){
+	err := priceGeneratorRepo.AddToStream(context.Background(), shares)
+	require.NoError(t, err)
+}
